076dd2b0eddc25df7c3592e94f4a6e32f334f952
vadym <vadim003600@gmail.com>
2026-05-22 16:57:08 -0500

    Created main file

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // Open every .yaml file inside _net_test
+    // Parse .yaml data into structs
+    //  - config
+    //  - target  
+    //  - test
+}
