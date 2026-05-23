16a46f396cef1dc64022dce41d37f9cfa1cd2060
vadym <vadim003600@gmail.com>
2026-05-23 15:06:51 -0500

    Defined initial network testing structures:
    target: stores target server information.
    test_case: represents a single test case, which can consist of binary input, text input, or hexadecimal input. Also stores the expected server response for validation.
    iso_test_node: represents an isolated collection of test cases executed in a separate process from other test groups.

---
+++ src/nettest_private.h
@@ -0,0 +1,44 @@
+#ifndef _TARGET_
+#define _TARGET_
+
+#include <stdint.h>
+#include <stddef.h>
+#include <netinet/in.h>
+
+#define PROTOCOL_TCP    1
+#define PROTOCOL_UDP    2
+#define IPV4            3
+#define IPV6            4
+
+enum test_case_type
+{
+    BIN,
+    HEX,
+    TEXT
+};
+
+struct target
+{
+    uint8_t protocol;
+    uint8_t family;
+    char ip_address[INET6_ADDRSTRLEN];
+    uint16_t port; 
+};
+
+struct test_case
+{
+    enum test_case_type case_t;
+    void *test_data; 
+    void *expected_output;
+    size_t test_data_size; 
+    size_t expected_size;
+};
+
+struct iso_test_node
+{
+    struct target target; 
+    struct test_case *test_cases;
+    size_t test_cases_num; 
+};
+
+#endif
