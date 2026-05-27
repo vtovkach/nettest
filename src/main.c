e5be3b7bf591f1eca6af1a77745e41682d47e2b5
V <vadim003600@gmail.com>
2026-05-27 12:19:00 -0500

    Add routine to add a new file path to the files array

--- src/main.c
+++ src/main.c
@@ -12,7 +12,30 @@ void free_files(char **files, size_t files_capacity)
     free(files);
 }
 
+int insert_new_file(char ***files, size_t *files_capacity, 
+                    const char *str, size_t str_idx )
+{
+    if(files == NULL || *files == NULL) return -1; 
+    if(files_capacity == NULL || *files_capacity == 0) return -1;
 
+    if(str_idx >= *files_capacity)
+    {
+        size_t new_capacity = (2 * (*files_capacity));
+
+        void **temp = realloc(*files, sizeof(char *) * new_capacity);
+        if(temp == NULL)
+            return -1;            
+
+        *files = (char **)temp;
+        *files_capacity = new_capacity;
+    }
+
+    (*files)[str_idx] = strdup(str);
+    if((*files)[str_idx] == NULL)
+        return -1;
+
+    return 0;
+}
 int main(int argc, char *argv[])
 {
     // Open every .yaml file inside _net_test
