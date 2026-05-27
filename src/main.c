328d4b95b4d3e0c888cdaa78ab6319e4362ee958
V <vadim003600@gmail.com>
2026-05-27 12:20:50 -0500

    update main function

--- src/main.c
+++ src/main.c
@@ -112,4 +112,30 @@ int main(int argc, char *argv[])
     //  - config
     //  - target  
     //  - test
+
+    // find and parse every .yaml file in _net_test/
+    // Create all necessary structures 
+    // Display status
+
+    // TODO: 
+    //  Enforce Max File and Dir Size
+    //  Fixed error messages 
+
+    if(argc != 2)
+    {
+        printf("main: incorrect args\n");
+        return 1;
+    }
+
+    char *tests_path = argv[1];
+    char **files = NULL; 
+
+    int num_files = retrieve_test_files(tests_path, &files); 
+    if(num_files < 1)
+    {
+        printf("Failed to open /_net_test\n");
+        return 1; 
+    }
+
+    return 0;
 }
