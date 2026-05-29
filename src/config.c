c5204c0a3ce4e7ce8582776b82691088f1a98056
V <vadim003600@gmail.com>
2026-05-29 11:11:08 -0500

    Initialize parse_file routine.

--- src/config.c
+++ src/config.c
@@ -4,6 +4,47 @@
 
 #include "config.h"
 
+static int parse_file(const char *file, struct iso_test_node *arr_dest, size_t dest_idx)
+{
+    if(!file)
+    {
+        printf("[parse_file] file is NULL\n");
+        return -1; 
+    }
+
+    FILE *yaml_file = fopen(file, "r");
+    if(!yaml_file)
+    {
+        perror("[parse_file] failed to open \"%s\" file:");
+        return -1;
+    }
+
+    yaml_parser_t parser; 
+    yaml_parser_set_input_file(&parser, yaml_file);
+
+    int stop = 0;
+    yaml_event_t event; 
+    while(!stop)
+    {
+        if(!yaml_parser_parse(&parser, &event))
+            goto error;
+
+        /* Process events here */
+        // ... 
+
+        /* Is everything parsed? */
+        stop = (event.type == YAML_STREAM_END_EVENT);
+        yaml_event_delete(&event);
+    }
+
+    yaml_parser_delete(&parser);
+    return 0; 
+
+    error:
+    yaml_parser_delete(&parser);
+    return -1;
+}
+
 int parse_yaml_files(char **files, size_t files_num, struct iso_test_node **dest)
 {
     if(!files) 
