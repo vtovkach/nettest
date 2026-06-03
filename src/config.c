a1b4f4753cc8f371c7d44bbb6a321f724ce10675
V <vadim003600@gmail.com>
2026-06-03 13:07:06 -0500

    Refactor parse_file function
    1. Moved switch block handling yaml events to a separate helper function:
        - handle_yaml_event(...)
    2. Moved yaml event's handling logic to the separate helping functions:
        - mapping_start(...)
        - mapping_end(...)
        - sequence_start(...)
        - sequence_end(...)
        - scalar_event(...)

--- src/config.c
+++ src/config.c
@@ -73,12 +73,12 @@ static int parse_port(const char *str, uint16_t *dest_port)
 static int parse_config(const char *cur_scalar, 
                         char *cur_key, 
                         struct target *target, 
-                        bool *expect_key)
+                        bool *isKey)
 {
-    if(*expect_key == true)
+    if(*isKey == true)
     {
         strcpy(cur_key, cur_scalar); // cur_scalar is guaranteed to be <KEY_MAX_SIZE 
-        *expect_key = false;
+        *isKey = false;
     }
     else
     {
@@ -129,7 +129,7 @@ static int parse_config(const char *cur_scalar,
             printf("Invalid scalar encountered: %s\n", cur_scalar);
             return -1;
         }
-        *expect_key = true;
+        *isKey = true;
     }
     return 0;
 }
@@ -170,179 +170,255 @@ static int parse_test_case(char *cur_key, const char *cur_scalar, bool *isKey, s
     return 0;
 }
 
+static int mapping_start(parse_state_t *parse_state)
+{
+    printf("YAML_MAPPING_START_EVENT\n");
+
+    if(*parse_state == STATE_STEPS)
+    {
+        *parse_state = STATE_TEST_CASE;
+    }
+
+    return 0;
+}
+
+static int mapping_end(parse_state_t *parse_state, struct test_file *t_file, 
+                       size_t *cur_test_idx, size_t *cur_node_idx)
+{
+    printf("YAML_MAPPING_END_EVENT\n");
+
+    if(*parse_state == STATE_CONFIG)
+    {
+        *parse_state = STATE_ROOT;
+    }
+    else if(*parse_state == STATE_TEST_CASE)
+    {
+        *parse_state = STATE_STEPS;
+        (*cur_test_idx)++;   
+        t_file->nodes[*cur_node_idx].tests_count++;
+    }
+    else if(*parse_state == STATE_TEST)
+    {
+        *parse_state = STATE_TESTS;
+    }
+    
+    return 0;
+}
+
+static int sequence_start(void)
+{
+    printf("YAML_SEQUENCE_START_EVENT\n");
+    return 0;
+}
+static int sequence_end(parse_state_t *parse_state, 
+                        struct test_file *t_file, size_t *cur_node_idx)
+{
+    printf("YAML_SEQUENCE_END_EVENT\n");
+
+    if(*parse_state == STATE_STEPS)
+    {
+        *parse_state = STATE_TEST;
+        (*cur_node_idx)++; 
+        t_file->nodes_count++;
+    }
+    else if(*parse_state == STATE_TESTS)
+    {
+        *parse_state = STATE_ROOT;
+    }
+    
+    return 0;
+}
+
+static int scalar_event(const yaml_event_t *event, parse_state_t *parse_state, 
+                        char *current_key, struct test_file *test_file, 
+                        bool *isKey, size_t *cur_node_idx, 
+                        size_t *cur_test_idx)
+{
+    char *value = (char *)event->data.scalar.value;
+    printf("YAML_SCALAR_EVENT: %s\n", value);
+
+    if(*parse_state == STATE_ROOT && (strcmp(value, "config") == 0))
+    {
+        *parse_state = STATE_CONFIG;
+    }
+    else if(*parse_state == STATE_CONFIG)
+    {
+        parse_config(value, current_key, &test_file->target, isKey);
+    }
+    else if(*parse_state == STATE_ROOT && (strcmp(value, "tests") == 0))
+    {
+        *parse_state = STATE_TESTS;
+    }
+    else if(*parse_state == STATE_TESTS && (strcmp(value, "test") == 0))
+    {
+        *parse_state = STATE_TEST;
+
+        if(test_file->nodes == NULL)
+        {
+            test_file->nodes = calloc(INIT_NODES_NUM, sizeof(struct test_node));
+            test_file->nodes_capacity = INIT_NODES_NUM;
+            if(!test_file->nodes) return -1;
+            *cur_node_idx = 0;
+        }
+        else if(*cur_node_idx >= test_file->nodes_capacity)
+        {
+            size_t new_capacity = test_file->nodes_capacity * 2; 
+            struct test_node *temp = realloc(test_file->nodes, new_capacity * sizeof(struct test_node));
+            if(!temp)
+            {
+                perror("[parse_file] realloc failure:");
+                return -1;
+            }
+            test_file->nodes = temp;
+            test_file->nodes_capacity = new_capacity;
+        }
+    }
+    else if(*parse_state == STATE_TEST && (strcmp(value, "steps") == 0))
+    {
+        *parse_state = STATE_STEPS;
+
+        *cur_test_idx = 0;
+        if(test_file->nodes[*cur_node_idx].test_cases == NULL)
+        {
+            test_file->nodes[*cur_node_idx].test_cases = calloc(INIT_TEST_CASES_NUM, sizeof(struct test_case));
+            test_file->nodes[*cur_node_idx].test_cases_capacity = INIT_TEST_CASES_NUM;
+        }
+    }
+    else if(*parse_state == STATE_TEST_CASE)
+    {
+        if(*cur_test_idx >= test_file->nodes[*cur_node_idx].test_cases_capacity)
+        {
+            size_t new_capacity = test_file->nodes[*cur_node_idx].test_cases_capacity * 2;
+            struct test_case *temp = realloc(test_file->nodes[*cur_node_idx].test_cases, sizeof(struct test_case) * new_capacity);
+            if(!temp)
+            {
+                perror("[parse_file] realloc failure:");
+                return -1;
+            }
+            test_file->nodes[*cur_node_idx].test_cases = temp;
+            test_file->nodes[*cur_node_idx].test_cases_capacity = new_capacity;
+        }
+
+        parse_test_case(current_key, value, isKey, &test_file->nodes[*cur_node_idx].test_cases[*cur_test_idx]);
+    }
+    
+    return 0;
+}
+
+static int handle_yaml_event(yaml_event_t *event, parse_state_t *parse_state,
+                            struct test_file *test_file, char *current_key,
+                            bool *isKey, size_t *cur_node_idx,
+                            size_t *cur_test_idx, int *stop)
+{
+    switch(event->type)
+    {
+        case YAML_MAPPING_START_EVENT:
+            return mapping_start(parse_state);
+    
+        case YAML_MAPPING_END_EVENT:
+            return mapping_end(parse_state, test_file, cur_test_idx, cur_node_idx);
+            
+        case YAML_SEQUENCE_START_EVENT:
+            return sequence_start();
+
+        case YAML_SEQUENCE_END_EVENT:
+            return sequence_end(parse_state, test_file, cur_node_idx);
+
+        case YAML_SCALAR_EVENT:
+            return scalar_event(event, parse_state, current_key, test_file, isKey, cur_node_idx, cur_test_idx);
+        
+        case YAML_STREAM_END_EVENT:
+            printf("YAML_STREAM_END_EVENT\n");
+            *stop = 1;
+            return 0;
+
+        default:
+            printf("%s\n", yaml_event_type_str(event->type));
+            return 0;
+    }
+}
+
 static int parse_file(const char *file, struct test_file *arr_dest, size_t dest_idx)
 {
-    if(!file)
-    {
-        printf("[parse_file] file is NULL\n");
-        return -1; 
-    }
+    FILE *yaml_file = NULL;
+    yaml_parser_t parser;
+    yaml_event_t event;
+    bool parser_initialized = false;
+    bool event_initialized = false;
+    int status = -1;
 
-    FILE *yaml_file = fopen(file, "r");
-    if(!yaml_file)
-    {
-        perror("[parse_file] failed to open \"%s\" file:");
-        return -1;
-    }
-
-    yaml_parser_t parser; 
-    if(!yaml_parser_initialize(&parser))
-    {
-        printf("[parse_file] yaml_parser_initialize failed");
-        fclose(yaml_file);
-        return -1;
-    }
-    yaml_parser_set_input_file(&parser, yaml_file);
-
-    // Node 
-    struct test_file test_file = {}; 
-    
-    // Parser State 
-    char current_key[KEY_MAX_SIZE];
+    struct test_file test_file = {0};
+    char current_key[KEY_MAX_SIZE] = {0};
     parse_state_t parse_state = STATE_ROOT;
-    bool expect_key = true;
-
-    test_file.nodes = NULL;   
+    bool is_key = true;
     size_t cur_node_idx = 0;
     size_t cur_test_idx = 0;
-
     int stop = 0;
-    yaml_event_t event; 
-    while(!stop)
+
+    if (!file || !arr_dest)
     {
-        if(!yaml_parser_parse(&parser, &event))
-            goto error;
-
-        switch(event.type)
-        {
-            case YAML_MAPPING_START_EVENT:
-            {
-                printf("YAML_MAPPING_START_EVENT\n");
-                if(parse_state == STATE_STEPS)
-                    parse_state = STATE_TEST_CASE;
-                break;
-            }
-            case YAML_MAPPING_END_EVENT:
-            {
-                printf("YAML_MAPPING_END_EVENT\n");
-                if(parse_state == STATE_CONFIG)
-                    parse_state = STATE_ROOT;
-                else if(parse_state == STATE_TEST_CASE)
-                {
-                    parse_state = STATE_STEPS;
-                    cur_test_idx++; 
-                    test_file.nodes[cur_node_idx].tests_count++;
-                }
-                else if(parse_state == STATE_TEST)
-                    parse_state = STATE_TESTS;
-                break;
-            }
-            case YAML_SEQUENCE_START_EVENT:
-            {
-                printf("YAML_SEQUENCE_START_EVENT\n");
-                break;
-            }
-            case YAML_SEQUENCE_END_EVENT:
-            {
-                printf("YAML_SEQUENCE_END_EVENT\n");
-                if(parse_state == STATE_STEPS)
-                {
-                    parse_state = STATE_TEST;
-                    cur_node_idx++; 
-                    test_file.nodes_count++; 
-                }
-                else if(parse_state == STATE_TESTS)
-                    parse_state = STATE_ROOT;
-                break;
-            }
-            case YAML_SCALAR_EVENT:
-            {
-                char *value = (char *)event.data.scalar.value;
-                printf("YAML_SCALAR_EVENT: %s\n", value);
-
-                if(parse_state == STATE_ROOT && (strcmp(value, "config") == 0))
-                    parse_state = STATE_CONFIG;
-                else if(parse_state == STATE_CONFIG)
-                    parse_config(value, current_key, &test_file.target, &expect_key);
-                else if(parse_state == STATE_ROOT && (strcmp(value, "tests") == 0))
-                    parse_state = STATE_TESTS;
-                else if(parse_state == STATE_TESTS && (strcmp(value, "test") == 0))
-                {
-                    parse_state = STATE_TEST;
-                    if(test_file.nodes == NULL)
-                    {
-                        test_file.nodes = calloc(INIT_NODES_NUM, sizeof(struct test_node));
-                        test_file.nodes_capacity = INIT_NODES_NUM;
-                        if(!test_file.nodes) goto error;
-                        cur_node_idx = 0;
-                    }
-                    else if(cur_node_idx >= test_file.nodes_capacity)
-                    {
-                        size_t new_capacity = test_file.nodes_capacity * 2; 
-                        struct test_node *temp = realloc(test_file.nodes, new_capacity * sizeof(struct test_node));
-                        if(!temp)
-                        {
-                            perror("[parse_file] realloc failure:");
-                            return -1;
-                        }
-                        test_file.nodes = temp;
-                        test_file.nodes_capacity = new_capacity;
-                    }
-                }
-                else if(parse_state == STATE_TEST && (strcmp(value, "steps") == 0))
-                {
-                    parse_state = STATE_STEPS;
-                    cur_test_idx = 0;
-                    if(test_file.nodes[cur_node_idx].test_cases == NULL)
-                    {
-                        test_file.nodes[cur_node_idx].test_cases = calloc(INIT_TEST_CASES_NUM, sizeof(struct test_case));
-                        test_file.nodes[cur_node_idx].test_cases_capacity = INIT_TEST_CASES_NUM;
-                    }
-                }
-                else if(parse_state == STATE_TEST_CASE)
-                {
-                    if(cur_test_idx >= test_file.nodes[cur_node_idx].test_cases_capacity)
-                    {
-                        size_t new_capacity = test_file.nodes[cur_node_idx].test_cases_capacity * 2;
-                        struct test_case *temp = realloc(test_file.nodes[cur_node_idx].test_cases, sizeof(struct test_case) * new_capacity);
-                        if(!temp)
-                        {
-                            perror("[parse_file] realloc failure:");
-                            return -1;
-                        }
-                        test_file.nodes[cur_node_idx].test_cases = temp;
-                        test_file.nodes[cur_node_idx].test_cases_capacity = new_capacity;
-                    }
-                    parse_test_case(current_key, value, &expect_key, &test_file.nodes[cur_node_idx].test_cases[cur_test_idx]);
-                }
-                break; 
-            }
-            case YAML_STREAM_END_EVENT:
-            {
-                printf("YAML_STREAM_END_EVENT\n");
-                stop = 1;
-                break;
-            }
-            default:
-            {
-                printf("%s\n", yaml_event_type_str(event.type));
-                break;
-            }
-        }
-        yaml_event_delete(&event);
+        fprintf(stderr, "[parse_file] invalid argument\n");
+        goto error;
     }
 
+    yaml_file = fopen(file, "r");
+    if (!yaml_file)
+    {
+        perror("[parse_file] fopen failed");
+        goto error;
+    }
+
+    if (!yaml_parser_initialize(&parser))
+    {
+        fprintf(stderr, "[parse_file] yaml_parser_initialize failed\n");
+        goto error;
+    }
+
+    parser_initialized = true;
+    yaml_parser_set_input_file(&parser, yaml_file);
+
+    while (!stop)
+    {
+        if (!yaml_parser_parse(&parser, &event))
+            goto error;
+
+        event_initialized = true;
+
+        status = handle_yaml_event(
+            &event,
+            &parse_state,
+            &test_file,
+            current_key,
+            &is_key,
+            &cur_node_idx,
+            &cur_test_idx,
+            &stop
+        );
+
+        yaml_event_delete(&event);
+        event_initialized = false;
+
+        if (status != 0)
+            goto error;
+    }
+
+    arr_dest[dest_idx] = test_file;
+
     yaml_parser_delete(&parser);
     fclose(yaml_file);
+    return 0;
 
-    memcpy(&arr_dest[dest_idx], &test_file, sizeof(struct test_file));
+error:
+    if (event_initialized)
+        yaml_event_delete(&event);
 
-    return 0; 
+    if (parser_initialized)
+        yaml_parser_delete(&parser);
 
-    error:
-    yaml_parser_delete(&parser);
-    fclose(yaml_file);
-    printf("[parse_file] yaml_parser_parse error\n");
+    if (yaml_file)
+        fclose(yaml_file);
+
+    fprintf(stderr, "[parse_file] failure\n");
     return -1;
 }
 
